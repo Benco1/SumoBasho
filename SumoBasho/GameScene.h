9878e48598e58667baf946d8e7382bd2ac99957a
Benco <bcos34@gmail.com>
2015-01-04 16:08:17 -0500

    Initial Commit

---
+++ SumoBasho/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  SumoBasho
+//
+
+//  Copyright (c) 2015 BencoMakes. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ SumoBasho/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  SumoBasho
+//
+
+//  Copyright (c) 2015 BencoMakes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
